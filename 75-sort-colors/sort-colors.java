6a2434daa0c668bd7d93b38386ce1918979082bb
Ashish Das <ashukanauji@gmail.com>
2025-05-25 01:31:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (50.01%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -12,27 +12,20 @@ class Solution {
             else
                 two++;
         }
-        int i=0;
-        while((zero!=0 || one!=0 || two!=0) && i<nums.length){
-            if(zero!=0){
+        
+        for(int i=0; i<nums.length;i++){
+            if(zero>0){
                 nums[i] = 0;
-                i++;
                 zero--;
-                continue;
             }
-            if(one!=0){
+            else if(one >0){
                 nums[i] = 1;
-                i++;
                 one--;
-                continue;
             }
-            if(two!=0){
+            else{
                 nums[i] = 2;
-                i++;
                 two--;
-                continue;
             }
         }
-        return;
     }
 }
