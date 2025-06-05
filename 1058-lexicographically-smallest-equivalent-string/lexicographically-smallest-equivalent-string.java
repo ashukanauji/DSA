1f7c239998c2f1f002c35832b54f93e6a65f4ee3
Ashish Das <ashukanauji@gmail.com>
2025-06-06 02:07:12 +0530

    Time: 2 ms (96.88%) | Memory: 41.7 MB (97.36%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.java
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.java
@@ -1,41 +1,38 @@
 class Solution {
+    int[] par = new int[26];
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
-        Map<Character, List<Character>> adj = new HashMap<>();
-        int n = s1.length();
-
-        // Build the adjacency list
-        for (int i = 0; i < n; i++) {
-            char u = s1.charAt(i);
-            char v = s2.charAt(i);
-
-            adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
-            adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+        for (int i = 0; i < 26; i++) {
+            par[i] = i;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            union(s1.charAt(i) - 'a', s2.charAt(i) - 'a');
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < baseStr.length(); i++) {
+            char ch = baseStr.charAt(i);
+            int root = find(ch - 'a');
+            sb.append((char)(root + 'a'));
         }
 
-        StringBuilder result = new StringBuilder();
-
-        for (char ch : baseStr.toCharArray()) {
-            boolean[] visited = new boolean[26];
-            char minChar = dfs(adj, ch, visited);
-            result.append(minChar);
-        }
-
-        return result.toString();
+        return sb.toString();
     }
 
-    private char dfs(Map<Character, List<Character>> adj, char ch, boolean[] visited) {
-        visited[ch - 'a'] = true;
-        char minChar = ch;
-
-        for (char neighbor : adj.getOrDefault(ch, new ArrayList<>())) {
-            if (!visited[neighbor - 'a']) {
-                char candidate = dfs(adj, neighbor, visited);
-                if (candidate < minChar) {
-                    minChar = candidate;
-                }
-            }
+    public int find(int x) {
+        if (par[x] != x) {
+            par[x] = find(par[x]);
         }
+        return par[x];
+    }
 
-        return minChar;
+    public void union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px == py) return;
+        // Always attach the larger to the smaller to maintain lex smallest
+        if (px < py) {
+            par[py] = px;
+        } else {
+            par[px] = py;
+        }
     }
 }
