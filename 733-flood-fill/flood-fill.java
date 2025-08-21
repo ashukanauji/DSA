9bbfbc3c1cbed304635c1829ba8e48377f744bec
Ashish Das <ashukanauji@gmail.com>
2025-08-21 17:55:35 +0530

    Time: 1 ms (44.33%) | Memory: 45.2 MB (32.67%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[][] dfs(int sr, int sc, int color, int[][] image, int[][] vis, int toColor){
+        int m = image.length;
+        int n = image[0].length;
+        int[] dRow = {0, -1, 0, 1};
+        int[] dCol = {1, 0, -1, 0};
+        vis[sr][sc] = color;
+
+        for(int i = 0; i < 4; i++){
+            int nRow = sr + dRow[i];
+            int nCol = sc + dCol[i];
+            if(nRow >=0 && nRow < m && nCol < n && nCol >= 0 && vis[nRow][nCol] != color && image[nRow][nCol] == toColor){
+                // vis[nRow][nCol] = color;
+                dfs(nRow, nCol, color, image, vis, toColor);
+            }
+        }
+
+        return vis;
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int m = image.length;
+        int n = image[0].length;
+        int toColor = image[sr][sc];
+
+        int[][] vis = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                // if(image[i][j] != color)
+                vis[i][j] = image[i][j];
+            }
+        }
+        
+        vis = dfs(sr, sc, color, image, vis, toColor);
+  
+        return vis;
+        
+    }
+}
